3b16d602a4041e9b2861c4c672ec4930b14b0a2f
Galina Mordovina <glukoloid@gmail.com>
2026-06-24 21:03:38 +0500

    Configure JWT authentication and current user endpoint

--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -11,3 +12,21 @@ class HealthCheckView(APIView):
     def get(self, request):
         _ = request  # чтобы не висело предупреждения (в след ветке продолжу)
         return Response({"status": "ok"})
+
+
+class CurrentUserView(APIView):
+    """Получение информации о текущем пользователе."""
+
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+
+        return Response(
+            {
+                "id": user.id,
+                "username": user.username,
+                "email": user.email,
+                "role": user.role,
+            }
+        )
